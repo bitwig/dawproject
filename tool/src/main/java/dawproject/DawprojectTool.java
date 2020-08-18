0db72b4190f4cd4a30f5d36c1605e893e5028186
Claes Johanson <claes@bitwig.com>
2020-08-18 11:19:35 +0200

    forgot to stage the rest of the files

--- tool/src/main/java/dawproject/DawprojectTool.java
+++ tool/src/main/java/dawproject/DawprojectTool.java
@@ -1,4 +1,4 @@
-package PACKAGE_NAME;
+package dawproject;
 
 public class DawprojectTool
 {
--- tool/src/main/java/dawproject/importer/Importer.java
+++ tool/src/main/java/dawproject/importer/Importer.java
@@ -1,5 +1,13 @@
-package dawproject;
+package dawproject.importer;
+
+import java.io.File;
+
+import dawproject.Metadata;
+import dawproject.Project;
 
 public interface Importer
 {
+   public Project importProject(final File file);
+
+   public Metadata importMetadata(final File file);
 }
