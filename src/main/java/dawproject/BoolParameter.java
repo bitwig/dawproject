8b0a37119826e5c09c97fa471f734dd69107d098
Claes Johanson <claes@bitwig.com>
2021-03-19 15:08:19 +0100

    Updated .dawproject to use XML internally

--- src/main/java/dawproject/BoolParameter.java
+++ src/main/java/dawproject/BoolParameter.java
@@ -6,4 +6,14 @@ public class BoolParameter extends Parameter
 {
    @XmlAttribute
    public boolean value;
+
+   public BoolParameter(final boolean v)
+   {
+      value = value;
+   }
+
+   public BoolParameter()
+   {
+      value = false;
+   }
 }
