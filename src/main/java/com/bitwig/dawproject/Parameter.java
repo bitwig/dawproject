0f87f505455a74e5e2a982edd7abaceaa6be9347
Claes Johanson <claes@bitwig.com>
2021-03-29 16:45:41 +0200

    Import VST2 parameter + make the automation import work. Added parameterID to Parameter class

--- src/main/java/com/bitwig/dawproject/Parameter.java
+++ src/main/java/com/bitwig/dawproject/Parameter.java
@@ -1,5 +1,6 @@
 package com.bitwig.dawproject;
 
+import jakarta.xml.bind.annotation.XmlAttribute;
 import jakarta.xml.bind.annotation.XmlRootElement;
 import jakarta.xml.bind.annotation.XmlSeeAlso;
 
@@ -7,4 +8,8 @@ import jakarta.xml.bind.annotation.XmlSeeAlso;
 @XmlSeeAlso({RealParameter.class, BoolParameter.class, IntegerParameter.class, EnumParameter.class, TimeSignatureParameter.class})
 public class Parameter extends Referencable
 {
+
+   /** Parameter ID as used by VST2 (index), VST3(ParamID) */
+   @XmlAttribute(required = false)
+   public Integer parameterID;
 }
