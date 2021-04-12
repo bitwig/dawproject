6693b585dc61f4f81f8d6c7d40f12baa8c619972
Claes Johanson <claes@bitwig.com>
2021-04-12 08:27:36 +0000

    build & test

---
+++ .space.kts
@@ -0,0 +1,9 @@
+/**
+* JetBrains Space Automation
+* This Kotlin-script file lets you automate build activities
+* For more info, see https://www.jetbrains.com/help/space/automation.html
+*/
+
+job("Build and run tests") {
+   gradlew("openjdk:12", "build")
+}
