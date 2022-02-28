cf6466e320ec006e2e3831867e92408d8d32dfc8
Claes Johanson <claes@bitwig.com>
2022-02-28 09:47:12 +0100

    force Arranger.content to be of type Lanes so there is no type ambiguitiy of its children

--- src/main/java/com/bitwig/dawproject/Arrangement.java
+++ src/main/java/com/bitwig/dawproject/Arrangement.java
@@ -1,8 +1,8 @@
 package com.bitwig.dawproject;
 
+import com.bitwig.dawproject.timeline.Lanes;
 import com.bitwig.dawproject.timeline.Markers;
 import com.bitwig.dawproject.timeline.Points;
-import com.bitwig.dawproject.timeline.Timeline;
 import jakarta.xml.bind.annotation.XmlElement;
 import jakarta.xml.bind.annotation.XmlElementRef;
 import jakarta.xml.bind.annotation.XmlRootElement;
@@ -10,19 +10,19 @@ import jakarta.xml.bind.annotation.XmlRootElement;
 @XmlRootElement
 public class Arrangement extends Referenceable
 {
-   @XmlElementRef
-   public Timeline content;
-
    /** Automation data for time-signature inside this Arrangement */
-   @XmlElement(required = false)
+   @XmlElement(required = false, name = "timeSignature", type = Points.class)
    public Points timeSignature;
 
    /** Automation data for tempo inside this Arrangement, which will define the conversion between seconds and beats
     * at the root level. */
-   @XmlElement(required = false)
+   @XmlElement(required = false, name = "tempo", type = Points.class)
    public Points tempo;
 
    /** Cue markers inside this arrangement */
-   @XmlElement(required = false)
+   @XmlElement(required = false, name = "markers", type = Markers.class)
    public Markers markers;
+
+   @XmlElementRef(name = "content", type = Lanes.class)
+   public Lanes content;
 }
