c4a349a5dd80abe561c1aa9eb4fa78b6c6a6d2a3
Claes Johanson <claes@bitwig.com>
2022-03-01 15:04:13 +0100

    Clip can now define a timebase for its content and/or fades which is different from the outside scope

--- src/main/java/com/bitwig/dawproject/timeline/Clip.java
+++ src/main/java/com/bitwig/dawproject/timeline/Clip.java
@@ -8,9 +8,11 @@ import jakarta.xml.bind.annotation.XmlRootElement;
 @XmlRootElement(name = "clip")
 public class Clip
 {
+   /** Time on the parent timeline where this clips starts playing. */
    @XmlAttribute(required = true)
    public double time;
 
+   /** Duration on the parent timeline of this clip. */
    @XmlAttribute(required = true)
    public double duration;
 
@@ -20,15 +22,28 @@ public class Clip
    @XmlAttribute(required = false)
    public String color;
 
-   /** Time inside the target timeline where the clip starts playing. */
+   /** The Timebase used by the scope inside this timeline. This affects the content/reference, playStart, playStop,
+    *  loopStart, loopEnd but not time and duration which are using the timebase of the parent scope. */
+   @XmlAttribute(required = false)
+   public Timebase timebase;
+
+   /** Time inside the content timeline (or reference) where the clip starts playing. */
    @XmlAttribute(required = false)
    public Double playStart;
+
+   /** Time inside the content timeline (or reference) where the clip stops playing. */
+   @XmlAttribute(required = false)
+   public Double playStop;
 
    @XmlAttribute(required = false)
    public Double loopStart;
 
    @XmlAttribute(required = false)
    public Double loopEnd;
+
+   /** The Timebase used by the fadeInTime and fadeOutTime. */
+   @XmlAttribute(required = false)
+   public Timebase fadeTimebase;
 
    /** Duration of fade-in */
    @XmlAttribute(required = false)
@@ -38,9 +53,14 @@ public class Clip
    @XmlAttribute(required = false)
    public Double fadeOutTime;
 
+   /** Content Timeline this clip is playing. */
    @XmlElementRef(required = false)
    public Timeline content;
 
+   /**
+    * Reference to a Content Timeline this clip is playing, in case of linked/alias clips. You can use either content
+    * or reference for one clip, but not both.
+    */
    @XmlAttribute(required = false)
    @XmlIDREF
    public Timeline reference;
