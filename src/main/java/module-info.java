94f75c799d43396d5dae9e18acb96edcf4fa4221
git-moss <juergen.mossgraber@googlemail.com>
2022-04-11 22:31:04 +0200

    Fixed DawProject.streamEmbedded which indirectly closed the stream, therefore the returned stream was invalid. Needed to go back to jaxb-impl 3.0.0. 3.0.1/3.0.2 gives an error that the implementation cannot be found. So far no idea how to fix this, so let's go with 3.0.0 for the time being. Added missing access for jakarta.xml.bind to mapped classes.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,6 +4,11 @@ module com.bitwig.dawproject
     requires org.apache.commons.io;
 
 
+    opens com.bitwig.dawproject to jakarta.xml.bind;
+    opens com.bitwig.dawproject.device to jakarta.xml.bind;
+    opens com.bitwig.dawproject.timeline to jakarta.xml.bind;
+
+
     exports com.bitwig.dawproject;
     exports com.bitwig.dawproject.device;
     exports com.bitwig.dawproject.timeline;
