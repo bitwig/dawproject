42911343992fc91a52604cbee3021a03c51d4884
git-moss <juergen.mossgraber@googlemail.com>
2022-04-11 19:46:59 +0200

    Removed duplicated activation dependency in different versions. Set JAXB implementation to runtime only. Harmonized JUnit version between Maven / Gradle. Added module descriptor for modularized Java projects. Updated Maven compiler plugin to correctly support module building.

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module com.bitwig.dawproject
+{
+    requires jakarta.xml.bind;
+    requires org.apache.commons.io;
+
+
+    exports com.bitwig.dawproject;
+    exports com.bitwig.dawproject.device;
+    exports com.bitwig.dawproject.timeline;
+}
