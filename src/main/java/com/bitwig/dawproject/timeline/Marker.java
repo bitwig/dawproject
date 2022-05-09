60000a4345cd92cb3846bc748288c5faddc66536
Claes Johanson <claes@bitwig.com>
2022-05-09 16:04:25 +0200

    make Marker extend Nameable

--- src/main/java/com/bitwig/dawproject/timeline/Marker.java
+++ src/main/java/com/bitwig/dawproject/timeline/Marker.java
@@ -1,17 +1,12 @@
 package com.bitwig.dawproject.timeline;
 
+import com.bitwig.dawproject.Nameable;
 import jakarta.xml.bind.annotation.XmlAttribute;
 import jakarta.xml.bind.annotation.XmlRootElement;
 
 @XmlRootElement(name = "Marker")
-public class Marker
+public class Marker extends Nameable
 {
    @XmlAttribute(required = true)
    public double time;
-
-   @XmlAttribute(required = false)
-   public String name;
-
-   @XmlAttribute(required = false)
-   public String color;
 }
