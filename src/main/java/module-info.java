7129c09da383a4851b93941cba6f6d96b242ea71
Jürgen Moßgraber <juergen.mossgraber@googlemail.com>
2022-05-09 16:14:55 +0200

    Added support for using library with modularized applications. Fixed duplicated activation library issue. (#36)
    * Updated Gradle to 7.4. POM improvements.
    
    * Added missing commons-io. Set UTF-8 as POM file format. Added sources plugin.
    
    * Can never be null but needs to be checked for existence.
    
    * Removed duplicated activation dependency in different versions.
    Set JAXB implementation to runtime only.
    Harmonized JUnit version between Maven / Gradle.
    Added module descriptor for modularized Java projects.
    Updated Maven compiler plugin to correctly support module building.
    
    * Fixed DawProject.streamEmbedded which indirectly closed the stream, therefore the returned stream was invalid.
    Needed to go back to jaxb-impl 3.0.0. 3.0.1/3.0.2 gives an error that the implementation cannot be found. So far no idea how to fix this, so let's go with 3.0.0 for the time being.
    Added missing access for jakarta.xml.bind to mapped classes.
    
    * Also fixed JAXB version in Gradle configuration.

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module com.bitwig.dawproject
+{
+    requires jakarta.xml.bind;
+    requires org.apache.commons.io;
+
+
+    opens com.bitwig.dawproject to jakarta.xml.bind;
+    opens com.bitwig.dawproject.device to jakarta.xml.bind;
+    opens com.bitwig.dawproject.timeline to jakarta.xml.bind;
+
+
+    exports com.bitwig.dawproject;
+    exports com.bitwig.dawproject.device;
+    exports com.bitwig.dawproject.timeline;
+}
