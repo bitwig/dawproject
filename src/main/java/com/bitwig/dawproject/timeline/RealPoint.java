a83ce02a2e36fb429324dadc692a9990147550c8
Claes Johanson <claes@bitwig.com>
2022-06-14 11:18:52 +0200

    dawproject: Allow interpolation mode to be defined per-point for automation

--- src/main/java/com/bitwig/dawproject/timeline/RealPoint.java
+++ src/main/java/com/bitwig/dawproject/timeline/RealPoint.java
@@ -1,6 +1,7 @@
 package com.bitwig.dawproject.timeline;
 
 import com.bitwig.dawproject.DoubleAdapter;
+import com.bitwig.dawproject.Interpolation;
 import jakarta.xml.bind.annotation.XmlAttribute;
 import jakarta.xml.bind.annotation.XmlRootElement;
 import jakarta.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
@@ -11,4 +12,8 @@ public class RealPoint extends Point
    @XmlJavaTypeAdapter(DoubleAdapter.class)
    @XmlAttribute(required = true)
    public Double value;
+
+   /** Interpolation mode used for the segment starting at this point. Default to 'hold' when unspecified. */
+   @XmlAttribute(required = false)
+   public Interpolation interpolation;
 }
