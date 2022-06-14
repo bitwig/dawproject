f8fb94abcf3ebacf66f31d961742a9bc2a1b0c03
Claes Johanson <claes@bitwig.com>
2022-06-14 10:00:09 +0200

    The Video class no longer inherits Audio
    so the audio stream becomes optional

--- src/main/java/com/bitwig/dawproject/timeline/Video.java
+++ src/main/java/com/bitwig/dawproject/timeline/Video.java
@@ -1,9 +1,29 @@
 package com.bitwig.dawproject.timeline;
 
+import com.bitwig.dawproject.FileReference;
+import jakarta.xml.bind.annotation.XmlAttribute;
+import jakarta.xml.bind.annotation.XmlElement;
 import jakarta.xml.bind.annotation.XmlRootElement;
 
 @XmlRootElement(name = "Video")
-public class Video extends Audio
+public class Video extends Timeline
 {
+   @XmlElement(name = "File", required = true)
+   public FileReference file = new FileReference();
 
+   /** duration in seconds/beats (timebase) of video-file */
+   @XmlAttribute(required = true)
+   public double duration;
+
+   /** sample-rate of audio (if present) */
+   @XmlAttribute(required = false)
+   public int sampleRate;
+
+   /** number of channels of audio (1=mono..., if present) */
+   @XmlAttribute(required = false)
+   public int channels;
+
+   /** Playback algorithm used to warp audio (vendor-specific, if present) */
+   @XmlAttribute(required = false)
+   public String algorithm;
 }
