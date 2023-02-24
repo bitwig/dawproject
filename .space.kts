7b1178c67f813949ef2bd007acbc672cb938ec38
Claes Johanson <claes@bitwig.com>
2023-02-24 18:28:35 +0100

    Revert "test"
    This reverts commit 24020d5051b8c4d52f11b0a3ba5993a3caa89cad.

--- .space.kts
+++ .space.kts
@@ -1,12 +1,6 @@
 job("Build and run tests") {
     container(displayName = "Gradle build", image = "openjdk:15") {
-        shellScript {
-            content = """
-                echo Hello
-                echo World!
-            """
-        }
-      kotlinScript { api ->
+        kotlinScript { api ->
             // here goes complex logic
             api.gradlew("build")
         }
