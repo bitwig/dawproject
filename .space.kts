24020d5051b8c4d52f11b0a3ba5993a3caa89cad
Claes Johanson <claes@bitwig.com>
2023-02-24 17:11:51 +0000

    test

--- .space.kts
+++ .space.kts
@@ -1,6 +1,12 @@
 job("Build and run tests") {
     container(displayName = "Gradle build", image = "openjdk:15") {
-        kotlinScript { api ->
+        shellScript {
+            content = """
+                echo Hello
+                echo World!
+            """
+        }
+      kotlinScript { api ->
             // here goes complex logic
             api.gradlew("build")
         }
