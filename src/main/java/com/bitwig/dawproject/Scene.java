bf0de21280cb19219f1258b4e3c55c3cb17d3927
Claes Johanson <claes@bitwig.com>
2023-06-30 11:26:40 +0200

    Scene structure comment

--- src/main/java/com/bitwig/dawproject/Scene.java
+++ src/main/java/com/bitwig/dawproject/Scene.java
@@ -8,7 +8,20 @@ import jakarta.xml.bind.annotation.XmlRootElement;
 @XmlRootElement(name = "Scene")
 public class Scene extends Referenceable
 {
-   /** Content timeline of this scene. */
+   /** Content timeline of this scene, will typically be structured like this:
+    * <pre>{@code
+    * <Scene>
+    *   <Lanes>
+    *     <ClipSlot track="...">
+    *        <Clip>
+    *           ...
+    *        </Clip>
+    *     </ClipSlot>
+    *      ...
+    *   </Lanes>
+    * </Scene>
+    * }</pre>
+    * */
    @XmlElementRef(name = "Timeline")
    public Timeline content;
 }
